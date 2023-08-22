e0a716d9d39f815ef5ed58fdf0171379aa448e45
Umm <thdwjddma@naver.com>
2023-08-22 18:58:13 +0900

    storyboard 연결완료

---
+++ MemoApp/Controller/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  MemoApp
+//
+//  Created by  on 2023/08/22.
+//
+
+import UIKit
+
+class TabBarController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ MemoApp/Model/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  MemoApp
+//
+//  Created by  on 2023/08/22.
+//
+
+import UIKit
+
+struct Data {
+    let title: String
+    let detail: String
+    let completion: Bool
+    let date: Int
+    let categories: [String: String] = [:]
+}
